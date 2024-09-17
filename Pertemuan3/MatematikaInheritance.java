43011d798012358c10fc34740543f418115faa74
Helmy Krisdin Garcia <148613373+Helmy0-0@users.noreply.github.com>
2024-09-17 23:13:29 +0700

    Add files via upload

---
+++ Pertemuan3/MatematikaInheritance.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan3;
+
+/**
+ *
+ * @author Helmy Krisdin Garcia
+ */
+public class MatematikaInheritance {
+    public static void main(String[] args) {
+        Matematika2 mtk = new Matematika2();
+        
+        mtk.pertambahan(10, 5);
+        mtk.pengurangan(10, 5);
+        mtk.perkalian(10, 5);
+        mtk.pembagian(10, 5);
+        mtk.modulus(10, 5);
+    }
+}
