922d7063172509a598a62ddcab520f40013f1662
Helmy Krisdin Garcia <148613373+Helmy0-0@users.noreply.github.com>
2024-09-17 23:18:24 +0700

    Add files via upload

---
+++ Pertemuan2/Matematika.java
@@ -0,0 +1,29 @@
+package pertemuan2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Helmy Krisdin Garcia
+ */
+public class Matematika {
+    public void pertambahan(int a, int b){
+        int tambah = a+b;
+        System.out.println("Pertambahan:  "+ a + "+" + b + "="+tambah);
+    }
+    public void pengurangan(int a, int b){
+        int kurang = a-b;
+        System.out.println("Pengurangan: "+ a + "-" + b + "="+kurang);
+    }
+    public void perkalian(int a, int b){
+        int kali = a*b;
+        System.out.println("Perkalian: "+ a + "x" + b + "="+kali);
+    }
+    public void pembagian(int a, int b){
+        int bagi = a/b;
+        System.out.println("Pembagian: "+ a + "/" + b + "="+bagi);
+    }
+}
