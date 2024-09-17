d16af371885a8a7ae671d9c86efb196cb9fbb5d6
Helmy Krisdin Garcia <148613373+Helmy0-0@users.noreply.github.com>
2024-09-17 23:05:15 +0700

    Add files via upload

---
+++ Matematika2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan3;
+
+/**
+ *
+ * @author Helmy Krisdin Garcia
+ */
+public class Matematika2 extends Matematika {
+    public void modulus(int a, int b) {
+        int mod = a % b;
+        System.out.println("Modulus: " + a + " % " + b + " = " + mod);
+    }
+}
+
